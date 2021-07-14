6ec9ce2900889e0347a8bc059dcf1580d5816907
Harrison <54120231+novox187@users.noreply.github.com>
2021-07-14 05:09:24 -0500

    Delete Proyecto_Colegio directory

--- Proyecto_Colegio/carga.main.php
+++
@@ -1,7 +0,0 @@
-<script>
-    window.onload = function () {
-        var contenedor = document.getElementById('contenedor_carga');
-        contenedor.style.visibility = 'hidden';
-        contenedor.style.opacity = '0';
-    }
-</script>
